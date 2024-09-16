b0109f4b621ae8cd51482cfad50e4522367221cc
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-09-17 00:22:19 +0100

    fix: added the logic requeired and moved the content to the add module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
+from add_0 import add
 
-    Args:
-        a: first integer
-        b: second integer
+a=1
+b=2
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-add(1,2)
+print(add(a,b))
